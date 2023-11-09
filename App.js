9b563515e755b5ce6845fd1d44a4125ecbc523c8
fada2020 <hyeogjui4@gmail.com>
2023-11-09 09:23:02 +0900

    쓸데없는 짓

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Vibe from './Vibration';
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Vibe />;
+  // return (
+  //   <View style={styles.container}>
+  //     <Text style={styles.text}>Hello, I made a JooWeather app???</Text>
+  //     <StatusBar style="light" />
+  //   </View>
+  // );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize:28,
+    color: "red",
   },
 });
